554361615623a1504e0b5b8ca4ce2a76fdd7c192
zmess24 <zac.messinger@gmail.com>
2018-03-13 10:19:39 -0400

    added web3 + set up rinkbey network config

--- ethereum/truffle.js
+++ ethereum/truffle.js
@@ -1,9 +1,17 @@
+var HDWalletProvider = require('truffle-hdwallet-provider');
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: function() {
+        new HDWalletProvider('spot shop alcohol vintage print credit north frame tuition execute youth wasp', 'https://rinkeby.infura.io/2KgE38uh5rYNDiH8nwzY')
+      }, 
+      network_id: '4'
     }
   }
 };
